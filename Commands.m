ce2d7582f738e0feba0979effb87593406916ec9
Febin Sebastian <febin.vadakkan@gmail.com>
2025-04-26 21:06:04 +0200

    Commiting initial repo and Commands script

---
+++ Commands.m
@@ -0,0 +1,16 @@
+m = 3 * 5
+%used to comment
+m = m + 1 %assigning value to a variable
+y = m/2
+k = 8 - 2; %Semicolon helps to not display output
+k
+m = 3 * k
+y
+y = m/2
+%Upper arrow gives previous commands
+%All variables and values are shown in right side
+%-- 26/04/2025 20:53 --%
+load('Commands.mat')
+y = m/2
+
+a = 1 + 2
